eb6316f34d049c912ae2695f4d6ca340594dab5d
Dawid Bialokryty <dawid.bialokryty@gmail.com>
2025-06-11 00:05:14 +0200

    added edit and delete habit routes

--- habitcakesapp/blueprints/habits/routes.py
+++ habitcakesapp/blueprints/habits/routes.py
@@ -8,6 +8,7 @@ from habitcakesapp.app import db, bcrypt
 habits = Blueprint('habits', __name__, template_folder='templates')
 
 @habits.route('/habit', methods=['POST'])
+@login_required
 def habit():
     title = request.form.get('title')
     description = request.form.get('description')
@@ -20,3 +21,31 @@ def habit():
     db.session.commit()
 
     return redirect(url_for('core.dashboard'))
+
+@habits.route('/habit/edit/<int:habit_id>', methods=['GET', 'POST'])
+@login_required
+def edit_habit(habit_id):
+    habit = Habit.query.filter_by(hid=habit_id, user_id=current_user.uid).first_or_404()
+
+    if request.method == 'POST':
+        habit.title = request.form.get('title')
+        habit.description = request.form.get('description')
+        habit.repeat_frequency = request.form.get('repeat_frequency')
+        habit.repeat_day = ".".join(request.form.getlist('repeat_day'))
+        # updating habit in database
+        db.session.commit()
+        return redirect(url_for('core.dashboard'))
+
+    # for GET - rendering the edit form with current habit data
+    return render_template('habits/edit_habit.html', habit=habit)
+
+@habits.route("/habit/delete/<int:habit_id>")
+@login_required
+def delete(habit_id):
+    habit = Habit.query.filter_by(hid=habit_id, user_id=current_user.uid).first_or_404()
+    try:
+        db.session.delete(habit)
+        db.session.commit()
+        return redirect(url_for('core.dashboard'))
+    except Exception as e:
+        return f"ERROR:{e}"
